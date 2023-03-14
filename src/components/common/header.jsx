7c66abc3fc3e4d2fc15c5fc57f5da76a307d99de
Kalidas Rajeev <126052424+kalidasgrowthplug@users.noreply.github.com>
2023-03-14 14:40:25 +0530

    Add files via upload
    header

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../../assets/logo.svg";
+import { Input } from "antd";
+
+const { Search } = Input;
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 70px;
+  background: #263f61;
+  padding: 0px 20px;
+`;
+
+const LogoContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const SearchContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: white;
+`;
+
+const StyledSearch = styled(Search)`
+  .ant-input-wrapper {
+    height: 100%;
+  }
+  .ant-input {
+    height: 100%;
+    background: rgba(255, 255, 255, 0.06);
+    border: 1px solid rgba(255, 255, 255, 0.26);
+    color: white;
+    font-family: poppins;
+    ::placeholder {
+      color: white;
+    }
+  }
+  .ant-btn {
+    height: 100%;
+  }
+`;
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Flex>
+      <LogoContainer>
+        <img src={logo} height="44px" />
+      </LogoContainer>
+      <SearchContainer>
+        <div>
+          <StyledSearch
+            placeholder="Search Movies"
+            // onSearch={onSearch}
+            enterButton
+            style={{ width: "275px", height: "40px" }}
+          />
+        </div>
+
+        <div>Logout</div>
+      </SearchContainer>
+    </Flex>
+  );
 };
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "./common.css";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#FF5E62",
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
